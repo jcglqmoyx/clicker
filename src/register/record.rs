3f0c429198ef9d0e0f66afd7c46679e1a768bf14
Chintsai Hwo <jcglqmoyx@gmail.com>
2024-03-15 17:49:13 +0800

    update

--- src/register/record.rs
+++ src/register/record.rs
@@ -1,4 +1,6 @@
 use fltk::button;
+use fltk::dialog::{FileDialog, FileDialogOptions, FileDialogType};
+use fltk::dialog::FileDialogType::BrowseSaveFile;
 use fltk::prelude::{InputExt, WidgetExt};
 
 use crate::global::click::{COUNT_RECORD_INPUT, EVENTS};
@@ -9,5 +11,14 @@ pub(crate) fn on_clear_record_button_clicked(button: &mut button::Button) {
         EVENTS.clear();
         COUNT_RECORD_INPUT.set_value("0");
         play_audio("./resources/audio/clear_record_sound.mp3", 300);
+    });
+}
+
+pub(crate) fn on_save_record_button_clicked(button: &mut button::Button) {
+    button.set_callback(|_| {
+        let mut dialog = FileDialog::new(BrowseSaveFile);
+        dialog.set_option(FileDialogOptions::SaveAsConfirm);
+        dialog.show();
+        println!("filename: {:#?}", dialog.filename());
     });
 }
