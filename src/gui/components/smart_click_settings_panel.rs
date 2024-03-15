33e8dcf37592e238fe341532be37f6f1d25d0985
Chintsai Hwo <jcglqmoyx@gmail.com>
2024-03-15 22:51:43 +0800

    update

--- src/gui/components/smart_click_settings_panel.rs
+++ src/gui/components/smart_click_settings_panel.rs
@@ -2,12 +2,10 @@ use fltk::button::Button;
 use fltk::button::CheckButton;
 use fltk::frame::Frame;
 use fltk::group::{Pack, PackType};
-use fltk::input::Input;
-use fltk::prelude::{GroupExt, InputExt, WidgetBase, WidgetExt};
-use fltk::window::Window;
+use fltk::prelude::{GroupExt, WidgetBase, WidgetExt};
 
 use crate::global::click::COUNT_RECORD_INPUT;
-use crate::register::record::on_clear_record_button_clicked;
+use crate::register::record::{on_clear_record_button_clicked, on_save_record_button_clicked};
 use crate::register::smart_click::toggle_click_mode;
 
 pub unsafe fn smart_click_settings_panel() -> Pack {
@@ -35,39 +33,7 @@ pub unsafe fn smart_click_settings_panel() -> Pack {
     let mut clear_record_button = Button::new(50, 400, 60, 20, "Clear");
     on_clear_record_button_clicked(&mut clear_record_button);
     let mut save_record_button = Button::new(50, 450, 60, 20, "Save");
-
-
-    save_record_button.set_callback(move |_| {
-        let mut save_window = Window::new(200, 200, 300, 200, "Save");
-        let mut title_input = Input::new(20, 20, 260, 30, "Title:");
-        let mut description_input = Input::new(20, 60, 260, 30, "Description:");
-        let mut content_input = Input::new(20, 100, 260, 30, "Content:");
-        let mut save_button = Button::new(80, 150, 60, 30, "Save");
-        let mut cancel_button = Button::new(160, 150, 60, 30, "Cancel");
-
-        save_button.set_callback({
-            let mut save_window = save_window.clone();
-            move |_| {
-                let title = title_input.value();
-                let description = description_input.value();
-                let content = content_input.value();
-
-                // save_to_database(&title, &description, &content);
-
-                save_window.hide();
-            }
-        });
-
-        cancel_button.set_callback({
-            let mut save_window = save_window.clone();
-            move |_| {
-                save_window.hide();
-            }
-        });
-
-        save_window.end();
-        save_window.show();
-    });
+    on_save_record_button_clicked(&mut save_record_button);
 
     let load_button = Button::new(50, 500, 60, 20, "Load");
     record_operation_panel.add(&clear_record_button);
--- src/register/record.rs
+++ src/register/record.rs
@@ -1,5 +1,8 @@
 use fltk::button;
-use fltk::prelude::{GroupExt, InputExt, WidgetExt};
+use fltk::button::Button;
+use fltk::input::Input;
+use fltk::prelude::{GroupExt, InputExt, WidgetBase, WidgetExt};
+use fltk::window::Window;
 
 use crate::global::click::{COUNT_RECORD_INPUT, EVENTS};
 use crate::utils::audio::play_audio;
@@ -12,6 +15,36 @@ pub(crate) fn on_clear_record_button_clicked(button: &mut button::Button) {
     });
 }
 
-// pub(crate) fn on_save_record_button_clicked(button: &mut Button) {
+pub(crate) fn on_save_record_button_clicked(button: &mut Button) {
+    button.set_callback(move |_| {
+        let mut save_window = Window::new(200, 200, 300, 200, "Save");
+        let mut title_input = Input::new(20, 20, 260, 30, "Title:");
+        let mut description_input = Input::new(20, 60, 260, 30, "Description:");
+        let mut content_input = Input::new(20, 100, 260, 30, "Content:");
+        let mut save_button = Button::new(80, 150, 60, 30, "Save");
+        let mut cancel_button = Button::new(160, 150, 60, 30, "Cancel");
 
-// }
+        save_button.set_callback({
+            let mut save_window = save_window.clone();
+            move |_| {
+                let title = title_input.value();
+                let description = description_input.value();
+                let content = content_input.value();
+
+                // save_to_database(&title, &description, &content);
+
+                save_window.hide();
+            }
+        });
+
+        cancel_button.set_callback({
+            let mut save_window = save_window.clone();
+            move |_| {
+                save_window.hide();
+            }
+        });
+
+        save_window.end();
+        save_window.show();
+    });
+}
