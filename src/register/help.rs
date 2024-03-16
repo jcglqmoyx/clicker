4d0673d1cfd2a279e5b189e883b21005fb8ad517
Chintsai Hwo <jcglqmoyx@gmail.com>
2024-03-16 22:45:09 +0800

    update

--- src/register/help.rs
+++ src/register/help.rs
@@ -1,8 +1,14 @@
 use fltk::prelude::WidgetExt;
+use locale_config::Locale;
 
 pub(crate) fn on_help_button_clicked(button: &mut fltk::button::Button) {
     button.set_callback(move |_| {
-        let _ = webbrowser::open("http://www.rust-lang.org");
+        let current_locale = Locale::current();
+
+        println!("Current locale: {:?}", current_locale.as_ref());
+        // let language = current_locale
+        // println!("Current language: {}", language);
+        // let _ = webbrowser::open("http://www.rust-lang.org");
     });
 }
 
